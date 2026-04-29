29e44f0a95ef564e4d5caaf94f51300971edda4a
chenjia404 <chenjiablog@gmail.com>
2026-04-30 02:55:57 +0800

    chore: 升级 Go 依赖

--- update/update_test.go
+++ update/update_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestVerifySignature(t *testing.T) {
 	if v, e := VerifySignature("1.zip"); v != true {
-		t.Errorf(e.Error())
+		t.Errorf("%v", e)
 		t.Errorf("VerifySignature")
 	}
 }
